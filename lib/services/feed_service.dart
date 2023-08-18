e6932d309a6d947964e7c3e21d860aac53d2fc8b
xeladu <xeladu@outlook.de>
2023-08-18 18:36:27 +0200

    Add paging

--- lib/services/feed_service.dart
+++ lib/services/feed_service.dart
@@ -4,15 +4,37 @@ import 'package:flutter_blog_query/models/rss_feed_dto.dart';
 import 'package:http/http.dart' as http;
 
 class FeedService {
-  final String articleEndpoint =
-      "https://quickcoder.org/wp-json/wp/v2/posts?per_page=100&_fields=title,excerpt,date,link,status,type,tags";
-  final String categoryEndpoint =
-      "https://quickcoder.org/wp-json/wp/v2/tags?per_page=100&_fields=id,name&orderby=id";
+  // how many records are to be fetched with every request (1-100)
+  final int _pageSizeToQuery = 100;
+
+  // this value is set after the first request when the REST API tells us
+  // how many article pages there are
+  // https://developer.wordpress.org/rest-api/using-the-rest-api/pagination/
+  int _totalArticlePageCountReturnedByWordPress = 0;
+
+  // the name of the header field that contains the information from the
+  // REST API how many pages with the given page size there are
+  final String _pageCountHeaderField = "x-wp-totalpages";
+
+  final String _articleEndpoint =
+      "https://quickcoder.org/wp-json/wp/v2/posts?page=%page%&per_page=%pageSize%&_fields=title,excerpt,date,link,status,type,tags";
+  final String _categoryEndpoint =
+      "https://quickcoder.org/wp-json/wp/v2/tags?per_page=%pageSize%&_fields=id,name&orderby=id";
 
   Future<(List<RssFeedItem>, Set<String>)> getFeedItems() async {
-    final articles = await _downloadArticles();
+    var pageCounter = 0;
+    final allArticles = [];
+    var newArticles = [];
+
+    // get articles from all pages
+    do {
+      pageCounter++;
+      newArticles = (await _downloadArticlePage(pageCounter)) as List;
+      allArticles.addAll(newArticles);
+    } while (pageCounter < _totalArticlePageCountReturnedByWordPress);
+
     final categories = await _downloadCategories();
-    final items = _adjustFeedContent(articles, categories);
+    final items = _adjustFeedContent(allArticles, categories);
 
     return items;
   }
@@ -22,8 +44,10 @@ class FeedService {
       final headers = <String, String>{};
       headers["accept"] = "application/json";
 
-      final response =
-          await http.get(Uri.parse(categoryEndpoint), headers: headers);
+      final response = await http.get(
+          Uri.parse(_categoryEndpoint.replaceFirst(
+              "%pageSize%", _pageSizeToQuery.toString())),
+          headers: headers);
 
       if (response.statusCode != 200) {
         throw Exception("${response.statusCode}: ${response.body}");
@@ -34,17 +58,27 @@ class FeedService {
     }
   }
 
-  Future _downloadArticles() async {
+  Future _downloadArticlePage(int page) async {
     try {
       final headers = <String, String>{};
       headers["accept"] = "application/json";
 
-      final response =
-          await http.get(Uri.parse(articleEndpoint), headers: headers);
+      final response = await http.get(
+          Uri.parse(_articleEndpoint
+              .replaceFirst("%page%", page.toString())
+              .replaceFirst("%pageSize%", _pageSizeToQuery.toString())),
+          headers: headers);
 
       if (response.statusCode != 200) {
         throw Exception("${response.statusCode}: ${response.body}");
       }
+
+      // Find out how many article pages with the current page size there are
+      _totalArticlePageCountReturnedByWordPress =
+          response.headers.containsKey(_pageCountHeaderField)
+              ? int.parse(response.headers[_pageCountHeaderField]!)
+              : 0;
+
       return _parseJsonMapToMap(jsonDecode(response.body));
     } on Exception catch (ex) {
       throw Exception("Could not download and parse blog feed!\r\n$ex");

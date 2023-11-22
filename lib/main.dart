e1d594c9cfac256c11d1c617bc83418af9218719
xeladu <xeladu@outlook.de>
2023-11-23 00:02:21 +0100

    Fix url

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog_query/widgets/app_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const ProviderScope(child: AppWidget()));
 }
